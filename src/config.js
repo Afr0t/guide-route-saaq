ff4e1ea0c3225c086bba28d7fa3a8862652f3a9d
Guide Route <dev@local>
2026-05-26 14:48:34 -0400

    Lien de soutien Buy Me a Coffee (stayfocus)

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@
 //   export const SUPPORT_URL = "https://www.buymeacoffee.com/tonpseudo";
 //   export const SUPPORT_URL = "https://github.com/sponsors/tonpseudo";
 //   export const SUPPORT_URL = "https://paypal.me/tonpseudo";
-export const SUPPORT_URL = "https://ko-fi.com/tonpseudo";
+export const SUPPORT_URL = "https://www.buymeacoffee.com/stayfocus";
 
 // Texte du bouton de soutien.
 export const SUPPORT_LABEL = "❤️ Encourager le projet";
